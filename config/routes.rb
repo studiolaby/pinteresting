dc6cd4b518df79ba078fd55bb12dcb35f2536a1c
Maze <studiolaby@gmail.com>
2014-02-05 03:10:34 +0900

    creating home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Pinterseting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about" # creates about_path
+  
+  # get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
